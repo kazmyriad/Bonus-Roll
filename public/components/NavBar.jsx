b0c4c29bcfeaf80414a9d0592cd71295030d7382
Alyssa F <aiepillios1101@gmail.com>
2026-04-21 13:47:12 -0400

    Added React and Vite

---
+++ public/components/NavBar.jsx
@@ -0,0 +1,9 @@
+// Needed Tabs: Dice Settings, Profile, Dice roller
+// We will def need vite for User info within the components
+
+function Navbar() {
+    return <div>hello</div>
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Navbar />)
