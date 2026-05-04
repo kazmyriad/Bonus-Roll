be8db0c9f4ec978f880f047e2985dc66a39bd5f0
Alyssa F <aiepillios1101@gmail.com>
2026-05-04 14:05:52 -0400

    App.jsx takes sessions (from profile) and passes to children (roller and settings)

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
-// NEEDS to be imported to index.html, and is the main entry point for the app
-// Define page contents here
-// import {COMPONENT NAME} from ''
-
-import NavBar from './components/NavBar.jsx'
+import { useState } from "react";
+import NavBar from "./components/NavBar.jsx";
 import { useSelector } from "react-redux";
 import Roller from "./components/Roller.jsx";
 import Settings from "./components/Settings.jsx";
 import Profile from "./components/Profile.jsx";
 
+function App() {
+  const activeLink = useSelector((state) => state.nav.activeLink);
 
+  const [session, setSession] = useState({
+    acctId: "",
+    token: "",
+    selectedGameId: "",
+  });
 
-function App(){
-    const activeLink = useSelector((state) => state.nav.activeLink);
-
-    return(
-        <div>
-            <NavBar />
-            {activeLink === "roller" && <Roller />}
-            {activeLink === "settings" && <Settings />}
-            {activeLink === "profile" && <Profile />}
-        </div>
-    )
-    
+  return (
+    <div>
+      <NavBar />
+      {activeLink === "roller" && <Roller session={session} />}
+      {activeLink === "settings" && (
+        <Settings session={session} setSession={setSession} />
+      )}
+      {activeLink === "profile" && (
+        <Profile session={session} setSession={setSession} />
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
