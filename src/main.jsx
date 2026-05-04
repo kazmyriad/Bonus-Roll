a982c3145f93499b437670aa9a8d91a225ca64dd
Alyssa F <aiepillios1101@gmail.com>
2026-05-04 18:47:59 -0400

    Frontend.

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,4 @@
-// import { StrictMode } from 'react'
-import ReactDOM from "react-dom/client";
-import { createRoot } from 'react-dom/client'
-import { Provider } from "react-redux";
-import { store } from "./app/store";
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
-        <App />
-    </Provider>
-)
+createRoot(document.getElementById("root")).render(<App />);
