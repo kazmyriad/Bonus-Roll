7b633f5b9b4684be4ccbd898301aa9a405ed8fe3
saj <92606915+sajninja@users.noreply.github.com>
2026-05-05 16:53:24 -0400

    getting login/acc creation working

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -2,13 +2,13 @@ import {useState, useEffect, useMemo } from "react";
 import "../styles.css";
 
 const API_BASE = "http://127.0.0.1:3000";
-const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN ?? 'http://localhost:5173';
+// const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN ?? 'http://localhost:5173';
 
-function setCors(res) { 
-  res.setHeader('Access-Control-Allow-Origin', ALLOWED_ORIGIN);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-}
+// function setCors(res) { 
+//   res.setHeader('Access-Control-Allow-Origin', ALLOWED_ORIGIN);
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+// }
 
 
 async function apiRequest(path, options = {}) {
